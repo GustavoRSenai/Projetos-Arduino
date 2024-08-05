23d3e23102899802f199587b512481195e4cba6d
Gustavo Rodrigues <gustavorsenai@gmail.com>
2024-08-05 15:11:25 -0300

    identação da programação

--- piscar_led/piscar_led.ino
+++ piscar_led/piscar_led.ino
@@ -1,9 +1,18 @@
+//programação para piscar led
+//manda sinal forte para saida 7
+//aguarda 1 segundo
+//envia sinal baixo para a saida 7
+
 void setup() {
   // put your setup code here, to run once:
+pinMode(7,OUTPUT)  //porta 7 para definir led como saida
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+digitalWrite(7,HIGH);//sinal alto
+delay(1000);//espere 1 segundo
+digitalWrite(7,LOW);//sinal baixo
+delay(1000);//espere 1 segundo
 }
